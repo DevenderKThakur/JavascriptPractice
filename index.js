55240bd18a693e13c6d4a1b123b167adeb5de84d
DevenderKThakur <101546402+DevenderKThakur@users.noreply.github.com>
2022-10-24 15:54:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,3 +19,45 @@ function swapNumber(){
   console.log('print:-'+a+'and:-'+b);
 }
 // swapNumber();
+
+// wap to find that the number is positive,negative, zero 
+
+function checkNumber(){
+  let x = parseInt(prompt('enter the value'));
+  if(x>1){
+    console.log('the number is +ve');
+  }
+  else if(x<1 && x!==0){
+    console.log('the number is -ve');
+  }
+  else{
+    console.log('the number is zero');
+  }
+}
+// checkNumber();
+
+// wap to find that the number is prime/non-prime number 
+
+function findPrimeOrNot(){
+  let x = parseInt(prompt('enter the value'));
+  let flag = 0;
+  
+  if(x!==0){                       //this is so that the value enter is not an zero
+    for(let i=2;i<x;i++){
+    if(x%i==0){
+      flag=1;                                   //this is initializing the value of the flag to be one which represents non-prime number 
+    }
+  }
+  if(flag==1){
+    console.log('the number is non-prime number');
+  }
+  else{
+    console.log('the number is prime number');
+  }
+ }
+  else{
+    console.log('Invalid value ! please enter again');
+  }
+}
+
+// findPrimeOrNot();
